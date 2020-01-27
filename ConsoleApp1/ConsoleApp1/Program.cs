2be8d34663df5179103323a7e4221153637513ad
Boian Kuzmanov <kuzmanovb@abv.bg>
2020-01-27 18:19:30 +0200

    Add task

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var reader = new StreamReader("Text.txt");
+            using (reader)
+            {
+
+            }
+            
+            
+        }
+    }
+}
