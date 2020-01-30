41ccda75c1003fe0f8685689ee64832260fe84fa
Boian Kuzmanov <kuzmanovb@abv.bg>
2020-01-30 11:59:28 +0200

    Add task

---
+++ Custom Comparator/Custom Comparator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+        }
+    }
+}
