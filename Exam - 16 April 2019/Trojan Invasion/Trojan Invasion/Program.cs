26eb5243550ac60fb06141adad3928bc416bc943
Boian Kuzmanov <kuzmanovb@abv.bg>
2020-01-31 22:58:12 +0200

    Remove task

--- Exam - 16 April 2019/Trojan Invasion/Trojan Invasion/Program.cs
+++ Exam - 16 April 2019/Trojan Invasion/Trojan Invasion/Program.cs
@@ -11,50 +11,69 @@ namespace Trojan_Invasion
             int wavesNumber = int.Parse(Console.ReadLine());
 
             var platesInput = Console.ReadLine().Split().Select(int.Parse).ToList();
-            var plateQueue = new Queue<int>(platesInput);
+            var platesQueue = new Queue<int>(platesInput);
             var wavesStack = new Stack<int>();
+
+            int curentPlate = 0;
 
             for (int i = 1; i <= wavesNumber; i++)
             {
-                var curentWaves = Console.ReadLine().Split().Select(int.Parse).ToList();
-                AddWaves(wavesStack, curentWaves);
+                var wavesInput = Console.ReadLine().Split().Select(int.Parse).ToList();
                 if (i % 3 == 0)
                 {
-                    plateQueue.Enqueue(int.Parse(Console.ReadLine()));
+                    var newPlate = int.Parse(Console.ReadLine());
+                    platesQueue.Enqueue(newPlate);
                 }
-
-                while (plateQueue.Any())
+                if (platesQueue.Any())
                 {
-                    int attckPlate = plateQueue.Dequeue();
-
-                    
-                    if (wavesStack.Any())
-                    {
-
-                    }
-
+                    AddWaves(wavesStack, wavesInput);
                 }
 
+                while (wavesStack.Any())
+                {
+                    if (!platesQueue.Any())
+                    {
+                        break;
+                    }
+                    if (curentPlate == 0 && platesQueue.Any())
+                    {
+                        curentPlate = platesQueue.Dequeue();
+                    }
+                    int curentWaves = wavesStack.Pop();
+                    if (curentPlate > curentWaves)
+                    {
+                        curentPlate -= curentWaves;
+                    }
+                    else if (curentPlate < curentWaves)
+                    {
+                        var plateForReturn = curentWaves - curentPlate;
 
-
-
-
-
-
-
+                        wavesStack.Push(plateForReturn);
+                        curentPlate = 0;
+                    }
+                    else if (curentPlate == curentWaves)
+                    {
+                        curentPlate = 0;
+                    }
+                }
             }
 
-
-
-
-
-
+            if (wavesStack.Any())
+            {
+                Console.WriteLine("The Trojans successfully destroyed the Spartan defense.");
+                Console.WriteLine($"Warriors left: {string.Join(", ", wavesStack)}");
+            }
+            else if (platesQueue.Any())
+            {
+                Console.WriteLine("The Spartans successfully repulsed the Trojan attack.");
+                Console.WriteLine($"Plates left: {string.Join(", ", platesQueue)}");
+            }
 
         }
 
-        private static void AddWaves(Stack<int> wavesStack, List<int> curentWaves)
+        private static void AddWaves(Stack<int> wavesStack, List<int> wavesInput)
         {
-            foreach (var item in curentWaves)
+            foreach (var item in wavesInput)
             {
                 wavesStack.Push(item);
 
