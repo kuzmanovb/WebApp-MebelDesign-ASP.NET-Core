46b4710c6203c0cf0cc2631aa006f76f3238224c
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-02-13 16:31:21 +0200

    Add task

---
+++ ListyIterator/ListyIterator/ListyIterator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> store;
+        private int count;
+
+        public ListyIterator( List<T> input)
+        {
+            this.store = input;
+            this.count = 0;
+        }
+
+        public bool Move()
+        {
+            if (count < store.Count)
+            {
+                count++;
+                return true;
+            }
+
+            return false;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.store[count]);
+        }
+        public bool HasNext()
+        {
+            if (count <store.Count-1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.store)
+            {
+                yield return item;
+            }
+            
+            
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ ListyIterator/ListyIterator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var command = Console.ReadLine();
+            ListyIterator<string> newlistyIterator = null;
+            while (command != "END")
+            {
+               
+                if (command.StartsWith("Create"))
+                {
+                    var commandArr = command.Split();
+                    var listForCreat = commandArr.TakeLast(commandArr.Length - 1).ToList();
+                    newlistyIterator = new ListyIterator<string>(listForCreat);
+                }
+
+            }
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
