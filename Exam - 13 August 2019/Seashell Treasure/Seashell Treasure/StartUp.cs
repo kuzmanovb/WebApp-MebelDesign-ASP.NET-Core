c6197e5f922b6e1edb612685f776e552adbe154a
Boian Kuzmanov <kuzmanovb@abv.bg>
2020-02-14 14:59:35 +0200

    Add

---
+++ Exam - 13 August 2019/Seashell Treasure/Seashell Treasure/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seashell_Treasure
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
