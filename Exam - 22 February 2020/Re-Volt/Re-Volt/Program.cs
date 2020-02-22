888bf399da07dfee833391ecf9f3ea91f703e707
Boian Kuzmanov <kuzmanovb@abv.bg>
2020-02-22 15:32:03 +0200

    Add

--- Exam - 22 February 2020/Re-Volt/Re-Volt/Program.cs
+++ Exam - 22 February 2020/Re-Volt/Re-Volt/Program.cs
@@ -16,33 +16,18 @@ namespace Task_2
 
             var rowPlayer = 0;
             var colPlayer = 0;
-            //Fill Matrix and find player
-            for (int row = 0; row < rowAndCol; row++)
-            {
-                var input = Console.ReadLine().ToCharArray();
-                for (int col = 0; col < rowAndCol; col++)
-                {
-                    matrix[row, col] = input[col];
 
-                    if (input[col] == 'f')
-                    {
-                        rowPlayer = row;
-                        colPlayer = col;
-                        matrix[row, col] = '-';
-                    }
-                }
-            }
+            FillMatrix(rowAndCol, matrix, ref rowPlayer, ref colPlayer);
 
             var checkForWon = false;
 
             for (int i = 0; i < countCommand; i++)
             {
                 var command = Console.ReadLine();
-                var checkForOut = false;
                 var oldRowPlayer = rowPlayer;
                 var oldColPlayer = colPlayer;
 
-                MovePlayer(rowAndCol, ref rowPlayer, ref colPlayer, command, ref checkForOut);
+                MovePlayer(rowAndCol, ref rowPlayer, ref colPlayer, command);
 
                 if (matrix[rowPlayer, colPlayer] == 'F')
                 {
@@ -51,7 +36,7 @@ namespace Task_2
                 }
                 else if (matrix[rowPlayer, colPlayer] == 'B')
                 {
-                    MovePlayer(rowAndCol, ref rowPlayer, ref colPlayer, command, ref checkForOut);
+                    MovePlayer(rowAndCol, ref rowPlayer, ref colPlayer, command);
                 }
                 else if (matrix[rowPlayer, colPlayer] == 'T')
                 {
@@ -71,6 +56,13 @@ namespace Task_2
             {
                 Console.WriteLine("Player lost!");
             }
+
+            PrintMatrix(rowAndCol, matrix);
+
+        }
+
+        private static void PrintMatrix(int rowAndCol, char[,] matrix)
+        {
             for (int row = 0; row < rowAndCol; row++)
             {
                 for (int col = 0; col < rowAndCol; col++)
@@ -79,16 +71,32 @@ namespace Task_2
                 }
                 Console.WriteLine();
             }
-
-
-
-
         }
 
-        private static void MovePlayer(int rowAndCol, ref int rowPlayer, ref int colPlayer, string command, ref bool checkForOut)
+        private static void FillMatrix(int rowAndCol, char[,] matrix, ref int rowPlayer, ref int colPlayer)
+        {
+            for (int row = 0; row < rowAndCol; row++)
+            {
+                var input = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < rowAndCol; col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (input[col] == 'f')
+                    {
+                        rowPlayer = row;
+                        colPlayer = col;
+                        matrix[row, col] = '-';
+                    }
+                }
+            }
+        }
+
+        private static void MovePlayer(int rowAndCol, ref int rowPlayer, ref int colPlayer, string command)
         {
             switch (command)
             {
+                // Move Row
                 case "up":
                     if (rowPlayer - 1 >= 0)
                     {
@@ -108,12 +116,10 @@ namespace Task_2
                     else
                     {
                         rowPlayer = 0;
-                        checkForOut = true;
                     }
                     break;
 
-
-
+                //Move Col
                 case "left":
                     if (colPlayer - 1 >= 0)
                     {
@@ -122,7 +128,6 @@ namespace Task_2
                     else
                     {
                         colPlayer = rowAndCol - 1;
-                        checkForOut = true;
                     }
                     break;
                 case "right":
@@ -133,7 +138,6 @@ namespace Task_2
                     else
                     {
                         colPlayer = 0;
-                        checkForOut = true;
                     }
                     break;
 
