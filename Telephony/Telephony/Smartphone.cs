e7f6b7422575298998d1388101227f94a0dfcf17
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-03-12 16:12:46 +0200

    Add

---
+++ Telephony/Telephony/Smartphone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : ICalling, IBrowsing
+    {
+
+        public string PhoneNumber { get; private set; }
+
+        public string Sites { get; private set; }
+    }
+}
