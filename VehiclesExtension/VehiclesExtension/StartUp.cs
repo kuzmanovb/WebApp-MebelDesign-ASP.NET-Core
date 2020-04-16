386ea6a48bd981522f4080b82254cda73745f57e
Boian Kuzmanov <kuzmanovb@abv.bg>
2020-04-16 17:27:08 +0300

    Add

--- VehiclesExtension/VehiclesExtension/StartUp.cs
+++ VehiclesExtension/VehiclesExtension/StartUp.cs
@@ -1,4 +1,7 @@
-﻿using VehiclesExtension.Core;
+﻿using System;
+using System.Reflection;
+using VehiclesExtension.Core;
+using VehiclesExtension.Models;
 
 namespace VehiclesExtension
 {
@@ -6,8 +9,24 @@ namespace VehiclesExtension
     {
         static void Main(string[] args)
         {
-            var engine = new Engine();
-            engine.Run();
+            //var engine = new Engine();
+            //engine.Run();
+
+
+            
+            Type proba = typeof(Vehicle);
+            Type proba2 = Type.GetType("VehiclesExtension.Models.Vehicle");
+            Type proba3 = typeof(Car);
+
+
+            Type probaBase = proba.BaseType;
+            Type probaBase2 = proba3.BaseType;
+            Type[] probaInter = proba.GetInterfaces();
+            Type[] probaInter2 = proba3.GetInterfaces();
+
+            FieldInfo[] field = proba.GetFields();
+
+            ;
         }
     }
 }
