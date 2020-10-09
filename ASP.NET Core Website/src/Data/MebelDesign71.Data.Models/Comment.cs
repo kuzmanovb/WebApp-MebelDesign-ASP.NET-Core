1b602d10a489ce893a527974da56998ac06a13fb
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-10-10 01:54:25 +0300

    Revert "Revert "Create entities""
    This reverts commit 53a8d467c30b08c6cf943e523fb98f38987039a1.

---
+++ ASP.NET Core Website/src/Data/MebelDesign71.Data.Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace MebelDesign71.Data.Models
+{
+    using MebelDesign71.Data.Common.Models;
+
+    public class Comment : BaseDeletableModel<int>
+    {
+
+        public string Description { get; set; }
+
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
---
+++ ASP.NET Core Website/src/Data/MebelDesign71.Data.Models/Enums/Status.cs
@@ -0,0 +1,10 @@
+﻿namespace MebelDesign71.Data.Models
+{
+    public enum Status
+    {
+        Accepted = 1,
+        Processing = 2,
+        Ready = 3,
+
+    }
+}
