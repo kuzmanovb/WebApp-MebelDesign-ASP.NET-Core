d87aefa88267ce4a29605573cc4daffc4d599bde
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-10-10 02:23:07 +0300

    Added draft entities

--- ASP.NET Core Website/src/Data/MebelDesign71.Data/ApplicationDbContext.cs
+++ ASP.NET Core Website/src/Data/MebelDesign71.Data/ApplicationDbContext.cs
@@ -26,6 +26,16 @@
 
         public DbSet<Setting> Settings { get; set; }
 
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<TypeService> TypeServices { get; set; }
+
         public override int SaveChanges() => this.SaveChanges(true);
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
