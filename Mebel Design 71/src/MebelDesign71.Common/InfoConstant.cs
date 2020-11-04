f632f745332b52bd44fadd43a9041cf576959a31
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-04 17:12:37 +0200

    Updete Layout

---
+++ Mebel Design 71/src/MebelDesign71.Common/InfoConstant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MebelDesign71.Common
+{
+    public static class InfoConstant
+    {
+        public const string Sity = "гр.София";
+
+        public const string Address = "ул.Суходолска 64";
+
+        public const string FirstPhoneNumber = "0898 475 421";
+
+        public const string SecondPhoneNumber = "0887 386 101";
+
+        public const string Email = "mebel_71@mail.bg";
+
+
+    }
+}
