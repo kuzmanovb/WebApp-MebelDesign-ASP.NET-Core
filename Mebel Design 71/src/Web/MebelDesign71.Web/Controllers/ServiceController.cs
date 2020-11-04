f403e615d83696cd898674307e4c6a8861293ba9
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-05 01:49:08 +0200

    Add Service View

---
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Controllers/ServiceController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MebelDesign71.Web.Controllers
+{
+    public class ServiceController : BaseController
+    {
+
+        public IActionResult ServiceIndex()
+        {
+
+            return this.View();
+
+        }
+    }
+}
