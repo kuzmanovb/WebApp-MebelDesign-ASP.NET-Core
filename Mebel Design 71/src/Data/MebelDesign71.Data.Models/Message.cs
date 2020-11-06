d18afa83a8b9b807081e7497bc2970a9c83f270e
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-06 11:51:23 +0200

    Add Entities Message And Review

---
+++ Mebel Design 71/src/Data/MebelDesign71.Data.Models/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using MebelDesign71.Data.Common.Models;
+
+namespace MebelDesign71.Data.Models
+{
+    public class Message : BaseModel<string>
+    {
+
+        public Message()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Mebel Design 71/src/Data/MebelDesign71.Data.Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using MebelDesign71.Data.Common.Models;
+using System;
+
+namespace MebelDesign71.Data.Models
+{
+    public class Review : BaseDeletableModel<string>
+    {
+        public Review()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public int ImageId { get; set; }
+        public Image Image { get; set; }
+        public string Description { get; set; }
+
+    }
+}
