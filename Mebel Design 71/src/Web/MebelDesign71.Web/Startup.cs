3ed2534fea08bf1d05d102aa8c429935e31c8e97
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-10 23:59:18 +0200

    Add FilesService To Start Menu

--- Mebel Design 71/src/Web/MebelDesign71.Web/Startup.cs
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Startup.cs
@@ -65,6 +65,7 @@
             services.AddTransient<IEmailSender, NullMessageSender>();
             services.AddTransient<ISettingsService, SettingsService>();
             services.AddTransient<IContactsService, ContactsService>();
+            services.AddTransient<IFilesService, FilesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
