845e008d23a1c952efbe0d4b5de2cb66cc473139
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-14 02:04:16 +0200

    Update ReviewSeeder.cs

--- Mebel Design 71/src/Data/MebelDesign71.Data/Seeding/ReviewSeeder.cs
+++ Mebel Design 71/src/Data/MebelDesign71.Data/Seeding/ReviewSeeder.cs
@@ -1,6 +1,7 @@
 ﻿namespace MebelDesign71.Data.Seeding
 {
     using System;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using MebelDesign71.Data.Models;
@@ -8,8 +9,6 @@
 
     public class ReviewSeeder : ISeeder
     {
-        private const int IdToFirstPersoFromImageToReview = 2;
-        private const int IdToSecondPersoFromImageToReview = 3;
 
         public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
         {
@@ -18,16 +17,16 @@
                 return;
             }
 
-            var imageOneId = IdToFirstPersoFromImageToReview;
+            var imageOne = dbContext.ImageToReviews.Where(n => n.File.Name == "FirstPersonImageReview").First();
             var descriptionOne = "От заплануването на кухнята ни до изпълнението й, бе показан голям професионализъм. Изработена с качествени материали. Добър екип. Препоръчвам.";
             var nameOne = "сем. Йовчеви";
-            await dbContext.Reviews.AddAsync(new Review { Name = nameOne, Description = descriptionOne, ImageId = imageOneId});
+            await dbContext.Reviews.AddAsync(new Review { Name = nameOne, Description = descriptionOne, ImageId = imageOne.Id});
 
 
-            var imageTwoId = IdToSecondPersoFromImageToReview;
+            var imageTwo = dbContext.ImageToReviews.Where(n => n.File.Name == "SecondPersonImageReview").First();
             var descriptionTwo = "Благодарим за страхотната работа! След 5 месеца проучвате на фирми за мебели, Ви открихме и бихме Ви препоръчали на всички! Коректност, спазени срокове, функционални препоръки за мебелите - всичко беше на ниво! След гардероба и обзавеждането за склада и антрето, започваме да събираме идеи за хола и детската стая и със сигурност бихме поръчали отново при Вас!";
             var nameTwo = "Александър Малинов";
-            await dbContext.Reviews.AddAsync(new Review { Name = nameTwo, Description = descriptionTwo, ImageId = imageTwoId});
+            await dbContext.Reviews.AddAsync(new Review { Name = nameTwo, Description = descriptionTwo, ImageId = imageTwo.Id});
         }
     }
 }
