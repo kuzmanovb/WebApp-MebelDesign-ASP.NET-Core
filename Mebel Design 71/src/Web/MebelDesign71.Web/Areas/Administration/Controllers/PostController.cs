8896ff2716918bf3392a98e5f327b4d1955daaff
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-15 20:16:57 +0200

    Initial Add Admin Panel

---
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Areas/Administration/Controllers/PostController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MebelDesign71.Web.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Compose()
+        {
+            return View();
+        }
+
+        public IActionResult ReadMail()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Areas/Administration/Controllers/TodoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MebelDesign71.Web.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
