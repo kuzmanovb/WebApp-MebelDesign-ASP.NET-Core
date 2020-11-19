c7c4b18848edb2d53bc1b5bf13715e3abd4e62af
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-19 18:03:07 +0200

    Add Gallery View And LayoutGallery

---
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Areas/Administration/Controllers/AdminProjectsGalleryController.cs
@@ -0,0 +1,28 @@
+﻿namespace MebelDesign71.Web.Areas.Administration.Controllers
+{
+    using MebelDesign71.Services.Data.Contracts;
+    using MebelDesign71.Web.Controllers;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AdminProjectsGalleryController : AdministrationController
+    {
+        private readonly IProjectsService projectsService;
+
+        public AdminProjectsGalleryController(IProjectsService projectsService)
+        {
+            this.projectsService = projectsService;
+        }
+
+        public IActionResult Index()
+        {
+            this.ViewData["AllProjects"] = this.projectsService.GetAllProjects();
+
+            return this.View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Areas/Administration/Controllers/ProjectGalleriesController.cs
@@ -0,0 +1,19 @@
+﻿using MebelDesign71.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MebelDesign71.Web.Areas.Administration.Controllers
+{
+    public class ProjectGalleriesController : BaseController
+    {
+
+        public IActionResult Index()
+        {
+
+            return this.View();
+        }
+    }
+}
