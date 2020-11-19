e221caadf1f5078e4f7e9379369c39555ddce2d8
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-19 16:29:07 +0200

    Add Controll And View For ProjectGalleries

---
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Areas/Administration/Controllers/AdminProjectGalleriesController.cs
@@ -0,0 +1,18 @@
+﻿namespace MebelDesign71.Web.Areas.Administration.Controllers
+{
+    using MebelDesign71.Web.Controllers;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AdminProjectGalleriesController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return this.View();
+        }
+    }
+}
