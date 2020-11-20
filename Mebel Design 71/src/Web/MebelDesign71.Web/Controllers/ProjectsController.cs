6a0f2d8cbdbc5490df81c8750456fa09fe23efb8
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-21 01:59:30 +0200

    Create Projects View

--- Mebel Design 71/src/Web/MebelDesign71.Web/Controllers/ProjectsController.cs
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Controllers/ProjectsController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace MebelDesign71.Web.Controllers
+﻿namespace MebelDesign71.Web.Controllers
 {
+    using MebelDesign71.Services.Data.Contracts;
+    using Microsoft.AspNetCore.Mvc;
+
     public class ProjectsController : Controller
     {
+        private readonly IProjectsService projectsService;
+
+        public ProjectsController(IProjectsService projectsService)
+        {
+            this.projectsService = projectsService;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
+            this.ViewData["projects"] = this.projectsService.GetAllProjects();
+
             return this.View();
         }
 
