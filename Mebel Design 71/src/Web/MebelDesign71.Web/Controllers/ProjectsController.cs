7ed8715b0a468cdead828849178982119946bed4
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-21 18:00:15 +0200

    Create Gallery To Project

--- Mebel Design 71/src/Web/MebelDesign71.Web/Controllers/ProjectsController.cs
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Controllers/ProjectsController.cs
@@ -2,14 +2,18 @@
 {
     using MebelDesign71.Services.Data.Contracts;
     using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+    using System.Threading.Tasks;
 
     public class ProjectsController : Controller
     {
         private readonly IProjectsService projectsService;
+        private readonly IProjectsGalleryService projectsGalleryService;
 
-        public ProjectsController(IProjectsService projectsService)
+        public ProjectsController(IProjectsService projectsService, IProjectsGalleryService projectsGalleryService)
         {
             this.projectsService = projectsService;
+            this.projectsGalleryService = projectsGalleryService;
         }
 
         [HttpGet]
@@ -20,8 +24,21 @@
             return this.View();
         }
 
-        public IActionResult CurrentProject(string category)
+        public async Task<IActionResult> CurrentProject(int id)
         {
+            var currentProject = await this.projectsService.GetProjectById(id);
+
+            return this.View(currentProject);
+        }
+
+        public async Task<IActionResult> Gallery(int id, string projectName)
+        {
+            var galleryProject = await this.projectsGalleryService.GetGallery(id);
+            this.ViewData["gallery"] = galleryProject;
+            this.ViewData["projectName"] = projectName;
+            var proba = galleryProject.ToArray();
+            this.ViewData["foo"] = proba[0].ImagePath;
+
             return this.View();
         }
     }
