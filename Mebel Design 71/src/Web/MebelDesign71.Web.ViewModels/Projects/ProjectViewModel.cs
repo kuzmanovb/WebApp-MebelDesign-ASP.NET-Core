772bdf07f77628cb1a884d14031cefe539e8615f
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-22 21:57:42 +0200

    Update ProjectViewModel.cs

--- Mebel Design 71/src/Web/MebelDesign71.Web.ViewModels/Projects/ProjectViewModel.cs
+++ Mebel Design 71/src/Web/MebelDesign71.Web.ViewModels/Projects/ProjectViewModel.cs
@@ -1,13 +1,19 @@
-﻿namespace MebelDesign71.Web.ViewModels.Projects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MebelDesign71.Web.ViewModels.Projects
 {
     public class ProjectViewModel
     {
         public int Id { get; set; }
 
+
+        [Display(Name ="Име")]
         public string Name { get; set; }
 
+        [Display(Name = "Описание")]
         public string Description { get; set; }
 
+        [Display(Name = "Снимка")]
         public string HeadImage { get; set; }
 
         public string IsDeleted { get; set; }
