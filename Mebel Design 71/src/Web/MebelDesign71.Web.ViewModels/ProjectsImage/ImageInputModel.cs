a576715fd0a09fd8cd0976d2119b32bbde9daba5
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-22 21:54:27 +0200

    Update ImageInputModel.cs

--- Mebel Design 71/src/Web/MebelDesign71.Web.ViewModels/ProjectsImage/ImageInputModel.cs
+++ Mebel Design 71/src/Web/MebelDesign71.Web.ViewModels/ProjectsImage/ImageInputModel.cs
@@ -1,13 +1,17 @@
 ﻿namespace MebelDesign71.Web.ViewModels.ProjectsImage
 {
+    using System.ComponentModel.DataAnnotations;
+
     using Microsoft.AspNetCore.Http;
 
     public class ImageInputModel
     {
         public int ProjectId { get; set; }
 
+        [Display(Name = "Описание")]
         public string Description { get; set; }
 
+        [Display(Name ="Снимка")]
         public IFormFile ImageFile { get; set; }
 
     }
