423604f0f0e13e01535e02e0926bc289ce564454
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-23 00:28:57 +0200

    Create AdminServices View And Controller

---
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Areas/Administration/Controllers/AdminServicesController.cs
@@ -0,0 +1,17 @@
+﻿namespace MebelDesign71.Web.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AdminServicesController : AdministrationController
+    {
+
+        public IActionResult Index()
+        {
+            //var allProjects = this.projectsService.GetAllProjectsWithDeleted();
+
+            //this.ViewData["AllProjects"] = allProjects;
+
+            return this.View();
+        }
+    }
+}
