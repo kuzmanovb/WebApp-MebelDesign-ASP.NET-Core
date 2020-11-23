a0cb4146f91deada38a2dddf2152f53fc16d4957
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-23 15:41:06 +0200

    Add Create Service

--- Mebel Design 71/src/Services/MebelDesign71.Services.Data/ServicesService.cs
+++ Mebel Design 71/src/Services/MebelDesign71.Services.Data/ServicesService.cs
@@ -1,41 +1,137 @@
 ﻿namespace MebelDesign71.Services.Data
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
+    using MebelDesign71.Data.Common.Repositories;
+    using MebelDesign71.Data.Models;
     using MebelDesign71.Services.Data.Contracts;
     using MebelDesign71.Web.ViewModels.Service;
 
     public class ServicesService : IServicesService
     {
-        public IEnumerable<ServiceViewModel> GetAllService()
+        private readonly IDeletableEntityRepository<Service> dbService;
+        private readonly IFilesService filesService;
+
+        public ServicesService(IDeletableEntityRepository<Service> dbService, IFilesService filesService)
+        {
+            this.dbService = dbService;
+            this.filesService = filesService;
+        }
+
+        public IEnumerable<ServiceInputModel> GetAllService()
+        {
+            var allServices = this.dbService.All()
+                .Select(s => new ServiceInputModel
+                {
+                    Name = s.Name,
+                    Description = s.Description,
+                    ImagePath = RenameFilePath(s.HeadImage.FilePath),
+                    DocumentId = s.DocumentId,
+                    DocumentName = s.Document.Name,
+
+                })
+                .ToList();
+
+            return allServices;
+        }
+
+        public IEnumerable<ServiceInputModel> GetAllServiceWithDeleted()
+        {
+            var allServices = this.dbService.AllWithDeleted()
+               .Select(s => new ServiceInputModel
+               {
+                   Name = s.Name,
+                   Description = s.Description,
+                   ImagePath = RenameFilePath(s.HeadImage.FilePath),
+                   DocumentId = s.DocumentId,
+                   DocumentName = s.Document.Name,
+                   IsDeleted = s.IsDeleted,
+               })
+               .ToList();
+
+            return allServices;
+        }
+
+        public async Task<ServiceViewModel> GetServiceByIdToView(int id)
+        {
+            //var allServices = this.dbService.All().ToList();
+
+            //var allServicesView = new List<ServiceViewModel>();
+
+            //foreach (var service in allServices)
+            //{
+            //    var decriptionList = service.Description.Split(new char[] { '.', '-' }, System.StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            //    var newServiceViewModel = new ServiceViewModel
+            //    {
+            //        Name = service.Name,
+            //        Description = decriptionList,
+            //        HeadImageId = service.HeadImageId,
+            //        DocumentId = service.DocumentId,
+            //    };
+
+            //    allServicesView.Add(newServiceViewModel);
+            //}
+
+            //return allServicesView;
+
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<int> CreateService(ServiceInputModel input)
+        {
+            var headImageId = await this.filesService.UploadToFileSystem(input.HeadImage, "images\\serviceImages", "Service Hade Image");
+            string documentId = null;
+
+            if (input.Document != null)
+            {
+                documentId = await this.filesService.UploadToFileSystem(input.Document, "serviceDocuments", "Service " + input.Name + "document");
+            }
+
+            var newService = new Service
+            {
+                Name = input.Name,
+                Description = input.Description,
+                HeadImageId = headImageId,
+                DocumentId = documentId,
+            };
+
+            await this.dbService.AddAsync(newService);
+            await this.dbService.SaveChangesAsync();
+
+            return newService.Id;
+        }
+
+        public async Task UpdateService(ServiceInputModel input)
         {
             throw new System.NotImplementedException();
         }
 
-        public IEnumerable<ServiceViewModel> GetAllServiceWithDeleted()
+        public async Task ChangeIsDeleteService(int id)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<ServiceInputModel> GetServiceById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
 
-        public Task<int> Createservice(ServiceInputModel input)
+        private static string RenameFilePath(string fullPath)
         {
-            throw new System.NotImplementedException();
-        }
+            var oldString = "\\";
+            var newString = "/";
+            var replaceSlashInFullPath = fullPath.Replace(oldString, newString);
 
-        public Task UpdateService(ServiceInputModel input)
-        {
-            throw new System.NotImplementedException();
-        }
+            var getIndexStartWwwRoot = fullPath.IndexOf("wwwroot");
+            var lengthWwwroot = "wwwroot".Length;
 
-        public Task ChangeIsDeleteService(int id)
-        {
-            throw new System.NotImplementedException();
+            if (getIndexStartWwwRoot >= 0)
+            {
+
+                var pathForView = replaceSlashInFullPath.Substring(getIndexStartWwwRoot + lengthWwwroot);
+                return pathForView;
+            }
+
+            return replaceSlashInFullPath;
         }
     }
 }
