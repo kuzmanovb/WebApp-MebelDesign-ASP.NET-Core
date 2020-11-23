924603505f8d5a3c0a1b90f4e62b785fe0407654
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-23 14:10:03 +0200

    Remove Upload Document From Service

---
+++ Mebel Design 71/src/Data/MebelDesign71.Data/Migrations/20201123101145_RemoveUploadDocumentFromService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MebelDesign71.Data.Migrations
+{
+    public partial class RemoveUploadDocumentFromService : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Services_FileOnFileSystems_UploadDocumentId",
+                table: "Services");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Services_UploadDocumentId",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "UploadDocumentId",
+                table: "Services");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UploadDocumentId",
+                table: "Services",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_UploadDocumentId",
+                table: "Services",
+                column: "UploadDocumentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Services_FileOnFileSystems_UploadDocumentId",
+                table: "Services",
+                column: "UploadDocumentId",
+                principalTable: "FileOnFileSystems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
