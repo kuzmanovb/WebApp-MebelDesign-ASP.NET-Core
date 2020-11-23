8dcb87b1b0de677f9e37938a504b0908a7ee2765
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-23 14:22:44 +0200

    Update IFilesService.cs

--- Mebel Design 71/src/Services/MebelDesign71.Services.Data/Contracts/IFilesService.cs
+++ Mebel Design 71/src/Services/MebelDesign71.Services.Data/Contracts/IFilesService.cs
@@ -1,10 +1,8 @@
 ﻿namespace MebelDesign71.Services.Data
 {
-    using System.Collections.Generic;
     using System.Threading.Tasks;
+
     using MebelDesign71.Data.Models;
-    using MebelDesign71.Web.ViewModels.Files;
-    using MebelDesign71.Web.ViewModels.Information;
     using Microsoft.AspNetCore.Http;
 
     public interface IFilesService
