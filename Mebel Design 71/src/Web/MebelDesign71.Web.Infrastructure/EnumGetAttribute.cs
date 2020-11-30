b2652e9453a8f9d9e5848a50ccfc20a3abf3092c
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-30 16:27:28 +0200

    Create EnumGetAttribute.cs

---
+++ Mebel Design 71/src/Web/MebelDesign71.Web.Infrastructure/EnumGetAttribute.cs
@@ -0,0 +1,30 @@
+﻿namespace MebelDesign71.Web.Infrastructure
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+
+    public static class EnumGetAttribute
+    {
+        public static string GetDescription<TEnum>(this TEnum value)
+        where TEnum : struct, IConvertible // Enums do implement IConvertible
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException("TEnum is not an enum type :(");
+            }
+
+            var et = typeof(TEnum);
+            var name = Enum.GetName(et, value);
+
+            var result = et.GetField(name)
+                ?.GetCustomAttributes(false)
+                ?.OfType<DescriptionAttribute>()
+                ?.FirstOrDefault()
+                .Description
+            ;
+
+            return result;
+        }
+    }
+}
