e8ccb72466a0d49ea8d3841cc1f4e26c2edaefdc
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-11-30 16:28:40 +0200

    Create AdminOrders

---
+++ Mebel Design 71/src/Web/MebelDesign71.Web/Areas/Administration/Controllers/AdminOrdersController.cs
@@ -0,0 +1,58 @@
+﻿namespace MebelDesign71.Web.Areas.Administration.Controllers
+{
+    using System.Threading.Tasks;
+
+    using MebelDesign71.Data.Models;
+    using MebelDesign71.Services.Data.Contracts;
+    using MebelDesign71.Web.ViewModels.Orders;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AdminOrdersController : AdministrationController
+    {
+        private readonly IOrdersService ordersService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AdminOrdersController(IOrdersService ordersService, UserManager<ApplicationUser> userManager)
+        {
+            this.ordersService = ordersService;
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            this.ViewData["users"] = await this.userManager.Users.ToListAsync();
+            this.ViewData["orders"] = this.ordersService.GetAllOrders();
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string id)
+        {
+            this.ViewData["users"] = await this.userManager.Users.ToListAsync();
+            this.ViewData["orders"] = this.ordersService.GetOrdersByUserId(id);
+            return this.View();
+        }
+
+        public IActionResult UpdateOrder(string id)
+        {
+            var order = this.ordersService.GetOrderById(id);
+
+            return this.View(order);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateOrder(OrderViewModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            await this.ordersService.UpdateOrder(input);
+
+            return this.RedirectToAction("Index");
+        }
+    }
+}
