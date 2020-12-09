8e8736e3dd4dc57b6614d6e8b06803128fba047e
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-12-10 01:32:27 +0200

    Update MessageServiceTests.cs

--- Mebel Design 71/src/Tests/MebelDesign71.Services.Data.Tests/MessageServiceTests.cs
+++ Mebel Design 71/src/Tests/MebelDesign71.Services.Data.Tests/MessageServiceTests.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Linq;
     using System.Threading.Tasks;
-
+    using Ganss.XSS;
     using MebelDesign71.Data;
     using MebelDesign71.Data.Common.Repositories;
     using MebelDesign71.Data.Models;
@@ -15,197 +15,184 @@
 
     using Xunit;
 
-    public class MessageServiceTests
+    public class MessageServiceTests : IDisposable
     {
 
-        public class TemplateTest : IDisposable
+        private readonly IMessagesService messagesService;
+
+        private IDeletableEntityRepository<Message> messageRepository;
+        private IDeletableEntityRepository<SendMessage> sendMessageRepository;
+
+        private ApplicationDbContext connection;
+        private IHtmlSanitizer htmlSanitizer;
+
+        private MessageInputModel messageInputModel;
+        private SendMessageInputModel sendMessageInputModel;
+
+        public MessageServiceTests()
         {
-            private readonly IMessagesService messagesService;
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+              .UseInMemoryDatabase(databaseName: "TestDb").Options;
+            this.connection = new ApplicationDbContext(options);
 
-            private IDeletableEntityRepository<Message> messageRepository;
-            private IDeletableEntityRepository<SendMessage> sendMessageRepository;
+            this.messageRepository = new EfDeletableEntityRepository<Message>(this.connection);
+            this.sendMessageRepository = new EfDeletableEntityRepository<SendMessage>(this.connection);
 
-            private ApplicationDbContext connection;
+            this.htmlSanitizer = new HtmlSanitizer();
 
-            private MessageInputModel messageInputModel;
-            private SendMessageInputModel sendMessageInputModel;
+            this.InitializeFields();
 
-            public TemplateTest()
+            this.messagesService = new MessagesService(this.messageRepository, this.sendMessageRepository, this.htmlSanitizer);
+        }
+
+        public void Dispose()
+        {
+            this.connection.Database.EnsureDeleted();
+            this.connection.Dispose();
+        }
+
+        [Fact]
+        public async Task TestAddMessageAsync()
+        {
+            await this.messagesService.AddMessageAsync(this.messageInputModel);
+            var messageFirstName = this.messageRepository.All().FirstOrDefault().FirstName;
+
+            Assert.Equal("Test First Name", messageFirstName);
+        }
+
+        [Fact]
+        public async Task TestAddSendMessageAsync()
+        {
+            await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
+            var sendMessageToEmail = this.sendMessageRepository.All().FirstOrDefault().ToEmail;
+
+            Assert.Equal("Test Email", sendMessageToEmail);
+        }
+
+        [Fact]
+        public async Task TestGetAllMessages()
+        {
+            await this.messagesService.AddMessageAsync(this.messageInputModel);
+            await this.messagesService.AddMessageAsync(this.messageInputModel);
+            await this.messagesService.AddMessageAsync(this.messageInputModel);
+
+            var messageCount = this.messagesService.GetAllMessages().Count;
+
+            Assert.Equal(3, messageCount);
+        }
+
+        [Fact]
+        public async Task TestGetAllSendMessages()
+        {
+            await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
+            await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
+
+            var sendMessageCount = this.messagesService.GetAllSendMessages().Count;
+
+            Assert.Equal(2, sendMessageCount);
+        }
+
+        [Fact]
+        public async Task TestGetIsDeletedMessages()
+        {
+            var firstId = await this.messagesService.AddMessageAsync(this.messageInputModel);
+            var secondId = await this.messagesService.AddMessageAsync(this.messageInputModel);
+
+            await this.messagesService.DeleteMessageAsync(firstId);
+            await this.messagesService.DeleteMessageAsync(secondId);
+
+            var deletedMessages = this.messagesService.GetIsDeletedMessages();
+
+            Assert.Equal(2, deletedMessages.Count);
+        }
+
+        [Fact]
+        public async Task TestGetMessageById()
+        {
+            var id = await this.messagesService.AddMessageAsync(this.messageInputModel);
+
+            var expectMessage = this.messagesService.GetMessageById(id);
+
+            Assert.NotNull(expectMessage);
+        }
+
+        [Fact]
+        public async Task TestGetSendMessagesById()
+        {
+            var id = await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
+
+            var expectMessage = this.messagesService.GetSendMessageById(id);
+
+            Assert.NotNull(expectMessage);
+        }
+
+        [Fact]
+        public async Task TestDeleteMessageAsync()
+        {
+            var id = await this.messagesService.AddMessageAsync(this.messageInputModel);
+
+            await this.messagesService.DeleteMessageAsync(id);
+
+            var expectMessage = this.messageRepository.AllWithDeleted().Where(m => m.Id == id).FirstOrDefault();
+
+            Assert.True(expectMessage.IsDeleted);
+        }
+
+        [Fact]
+        public async Task TestDeleteSendMessageAsync()
+        {
+            var id = await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
+
+            await this.messagesService.DeleteSendMessageAsync(id);
+
+            var expectMessage = this.sendMessageRepository.AllWithDeleted().Where(m => m.Id == id).FirstOrDefault();
+
+            Assert.True(expectMessage.IsDeleted);
+        }
+
+        [Fact]
+        public async Task TestRestoreMessageAsync()
+        {
+            var id = await this.messagesService.AddMessageAsync(this.messageInputModel);
+
+            await this.messagesService.DeleteMessageAsync(id);
+
+            await this.messagesService.RestoreMessageAsync(id);
+
+            var expectMessage = this.messageRepository.All().Where(m => m.Id == id).FirstOrDefault();
+
+            Assert.NotNull(expectMessage);
+        }
+
+        [Fact]
+        public async Task TestHardDeleteMessageAsync()
+        {
+            var id = await this.messagesService.AddMessageAsync(this.messageInputModel);
+
+            await this.messagesService.HardDeleteMessageAsync(id);
+
+            var count = this.messageRepository.AllWithDeleted().ToList().Count;
+
+            Assert.Equal(0, count);
+        }
+
+        private void InitializeFields()
+        {
+            this.messageInputModel = new MessageInputModel
             {
-                var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                  .UseInMemoryDatabase(databaseName: "TestDb").Options;
-                this.connection = new ApplicationDbContext(options);
+                FirstName = "Test First Name",
+                LastName = "Test Last Name",
+                Email = "Test Email",
+                About = "Test About",
+                Description = "Test Description",
+            };
 
-                this.messageRepository = new EfDeletableEntityRepository<Message>(this.connection);
-                this.sendMessageRepository = new EfDeletableEntityRepository<SendMessage>(this.connection);
-
-                this.InitializeFields();
-
-                this.messagesService = new MessagesService(this.messageRepository, this.sendMessageRepository);
-            }
-
-            public void Dispose()
+            this.sendMessageInputModel = new SendMessageInputModel
             {
-                this.connection.Database.EnsureDeleted();
-                this.connection.Dispose();
-            }
-
-            // Start Testing -------------------------------------------------------------------------------------------------------
-
-            [Fact]
-            public async Task TestAddMessageAsync()
-            {
-                await this.messagesService.AddMessageAsync(this.messageInputModel);
-                var messageFirstName = this.messageRepository.All().FirstOrDefault().FirstName;
-
-                Assert.Equal("Test First Name", messageFirstName);
-            }
-
-            [Fact]
-            public async Task TestAddSendMessageAsync()
-            {
-                await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
-                var sendMessageToEmail = this.sendMessageRepository.All().FirstOrDefault().ToEmail;
-
-                Assert.Equal("Test Email", sendMessageToEmail);
-            }
-
-            [Fact]
-            public async Task TestGetAllMessages()
-            {
-                await this.messagesService.AddMessageAsync(this.messageInputModel);
-                await this.messagesService.AddMessageAsync(this.messageInputModel);
-                await this.messagesService.AddMessageAsync(this.messageInputModel);
-
-                var messageCount = this.messagesService.GetAllMessages().Count;
-
-                Assert.Equal(3, messageCount);
-            }
-
-            [Fact]
-            public async Task TestGetAllSendMessages()
-            {
-                await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
-                await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
-
-                var sendMessageCount = this.messagesService.GetAllSendMessages().Count;
-
-                Assert.Equal(2, sendMessageCount);
-            }
-
-            [Fact]
-            public async Task TestGetIsDeletedMessages()
-            {
-                var firstId = await this.messagesService.AddMessageAsync(this.messageInputModel);
-                var secondId = await this.messagesService.AddMessageAsync(this.messageInputModel);
-
-                await this.messagesService.DeleteMessageAsync(firstId);
-                await this.messagesService.DeleteMessageAsync(secondId);
-
-                var deletedMessages = this.messagesService.GetIsDeletedMessages();
-
-                Assert.Equal(2, deletedMessages.Count);
-            }
-
-            [Fact]
-            public async Task TestGetMessageById()
-            {
-                var id = await this.messagesService.AddMessageAsync(this.messageInputModel);
-
-                var expectMessage = this.messagesService.GetMessageById(id);
-
-                Assert.NotNull(expectMessage);
-            }
-
-            [Fact]
-            public async Task TestGetSendMessagesById()
-            {
-                var id = await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
-
-                var expectMessage = this.messagesService.GetSendMessageById(id);
-
-                Assert.NotNull(expectMessage);
-            }
-
-            [Fact]
-            public async Task TestDeleteMessageAsync()
-            {
-                var id = await this.messagesService.AddMessageAsync(this.messageInputModel);
-
-                await this.messagesService.DeleteMessageAsync(id);
-
-                var expectMessage = this.messageRepository.AllWithDeleted().Where(m => m.Id == id).FirstOrDefault();
-
-                Assert.True(expectMessage.IsDeleted);
-            }
-
-            [Fact]
-            public async Task TestDeleteSendMessageAsync()
-            {
-                var id = await this.messagesService.AddSendMessageAsync(this.sendMessageInputModel);
-
-                await this.messagesService.DeleteSendMessageAsync(id);
-
-                var expectMessage = this.sendMessageRepository.AllWithDeleted().Where(m => m.Id == id).FirstOrDefault();
-
-                Assert.True(expectMessage.IsDeleted);
-            }
-
-            [Fact]
-            public async Task TestRestoreMessageAsync()
-            {
-                var id = await this.messagesService.AddMessageAsync(this.messageInputModel);
-
-                await this.messagesService.DeleteMessageAsync(id);
-
-                await this.messagesService.RestoreMessageAsync(id);
-
-                var expectMessage = this.messageRepository.All().Where(m => m.Id == id).FirstOrDefault();
-
-                Assert.NotNull(expectMessage);
-            }
-
-            [Fact]
-            public async Task TestHardDeleteMessageAsync()
-            {
-                var id = await this.messagesService.AddMessageAsync(this.messageInputModel);
-
-                await this.messagesService.HardDeleteMessageAsync(id);
-
-                var count = this.messageRepository.AllWithDeleted().ToList().Count;
-
-                Assert.Equal(0, count);
-            }
-
-
-
-
-
-
-
-
-
-            // End Testing ---------------------------------------------------------------------------------------------------------------
-            private void InitializeFields()
-            {
-                this.messageInputModel = new MessageInputModel
-                {
-                    FirstName = "Test First Name",
-                    LastName = "Test Last Name",
-                    Email = "Test Email",
-                    About = "Test About",
-                    Description = "Test Description",
-                };
-
-                this.sendMessageInputModel = new SendMessageInputModel
-                {
-                    About = "Test About",
-                    Description = "Test Description",
-                    Email = "Test Email",
-                    ToMessageId = "Test MessageId",
-                };
-
-            }
+                About = "Test About",
+                Description = "Test Description",
+                Email = "Test Email",
+            };
         }
     }
 }
