a998561e77d4096b58db709592704ba94d424f5a
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2020-12-14 16:16:12 +0200

    Update InformationServiceTests.cs

--- Mebel Design 71/src/Tests/MebelDesign71.Services.Data.Tests/InformationServiceTests.cs
+++ Mebel Design 71/src/Tests/MebelDesign71.Services.Data.Tests/InformationServiceTests.cs
@@ -34,6 +34,8 @@
         private HostingEnvironment environment;
         private IHtmlSanitizer htmlSanitizer;
 
+        private IFormFile imageFile;
+
         public InformationServiceTests()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -49,6 +51,8 @@
 
             this.filesService = new FilesService(this.filesRepository, this.environment);
             this.informationService = new InformationService(this.imageRepository, this.reviewRepository, this.filesService, this.htmlSanitizer);
+
+            this.InitializeFields();
         }
 
         public void Dispose()
@@ -60,86 +64,40 @@
         [Fact]
         public async Task TestAddingReiewWithImageFile()
         {
-            var imageFileDefaultMock = new Mock<IFormFile>();
-            var defaultContent = "Hello World from a Fake File";
-            var defaultFileName = "DefaultImageReview.jpg";
-            var defaultMs = new MemoryStream();
-            var defautwriter = new StreamWriter(defaultMs);
-            defautwriter.Write(defaultContent);
-            defautwriter.Flush();
-            defaultMs.Position = 0;
-            imageFileDefaultMock.Setup(_ => _.OpenReadStream()).Returns(defaultMs);
-            imageFileDefaultMock.Setup(_ => _.FileName).Returns(defaultFileName);
-            imageFileDefaultMock.Setup(_ => _.Length).Returns(defaultMs.Length);
-
-            var imageFileDefault = imageFileDefaultMock.Object;
-
-            var imageFileMock = new Mock<IFormFile>();
-            var content = "Hello World from a Fake File";
-            var fileName = "test.jpg";
-            var ms = new MemoryStream();
-            var writer = new StreamWriter(ms);
-            writer.Write(content);
-            writer.Flush();
-            ms.Position = 0;
-            imageFileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
-            imageFileMock.Setup(_ => _.FileName).Returns(fileName);
-            imageFileMock.Setup(_ => _.Length).Returns(ms.Length);
-
-            var imageFile = imageFileMock.Object;
-
-            var fileId = await this.filesService.UploadToFileSystem(imageFileDefault, "test");
-            await this.imageRepository.AddAsync(new ImageToReview { FileId = fileId });
-            await this.imageRepository.SaveChangesAsync();
+            await this.AddDefaultImage();
 
             var model = new ReviewInputModel
             {
                 Name = "Test",
                 Description = "Lorem Ipsum is simply dummy text.",
-                ImageFile = imageFile,
+                ImageFile = this.imageFile,
+                UserId = "test",
             };
 
             await this.informationService.AddRewievAsync(model);
 
             var currentModel = await this.reviewRepository.All().FirstOrDefaultAsync();
 
-            Assert.Equal("Test", currentModel.Name);
-            Assert.Equal("Lorem Ipsum is simply dummy text.", currentModel.Description);
+            Assert.NotNull(currentModel);
         }
 
         [Fact]
         public async Task TestAddingReiewWithoutImage()
         {
-            var imageFileDefaultMock = new Mock<IFormFile>();
-            var defaultContent = "Hello World from a Fake File";
-            var defaultFileName = "DefaultImageReview.jpg";
-            var defaultMs = new MemoryStream();
-            var defautwriter = new StreamWriter(defaultMs);
-            defautwriter.Write(defaultContent);
-            defautwriter.Flush();
-            defaultMs.Position = 0;
-            imageFileDefaultMock.Setup(_ => _.OpenReadStream()).Returns(defaultMs);
-            imageFileDefaultMock.Setup(_ => _.FileName).Returns(defaultFileName);
-            imageFileDefaultMock.Setup(_ => _.Length).Returns(defaultMs.Length);
-
-            var imageFileDefault = imageFileDefaultMock.Object;
-
-            var fileId = await this.filesService.UploadToFileSystem(imageFileDefault, "test");
-            await this.imageRepository.AddAsync(new ImageToReview { FileId = fileId });
-            await this.imageRepository.SaveChangesAsync();
+            await this.AddDefaultImage();
 
             var model = new ReviewInputModel
             {
                 Name = "Test",
                 Description = "Lorem Ipsum is simply dummy text.",
+                UserId = "test",
             };
 
             await this.informationService.AddRewievAsync(model);
 
             var currentModel = await this.reviewRepository.All().FirstOrDefaultAsync();
 
-            Assert.Equal("Test", currentModel.Name);
-            Assert.Equal("Lorem Ipsum is simply dummy text.", currentModel.Description);
+            Assert.NotNull(currentModel);
         }
 
         [Fact]
@@ -153,6 +111,29 @@
         [Fact]
         public async Task TestDeleteRewiev()
         {
+            await this.AddDefaultImage();
+
+            var model = new ReviewInputModel
+            {
+                Name = "Test",
+                Description = "Lorem Ipsum is simply dummy text.",
+                ImageFile = this.imageFile,
+                UserId = "test",
+            };
+
+            await this.informationService.AddRewievAsync(model);
+
+            var currentModel = await this.reviewRepository.All().FirstOrDefaultAsync();
+
+            await this.informationService.DeleteReviewAsync(currentModel.Id);
+
+            var countReview = await this.informationService.GetAllReviewAsync();
+
+            Assert.Equal(0, countReview.Count);
+        }
+
+        private async Task AddDefaultImage()
+        {
             var imageFileDefaultMock = new Mock<IFormFile>();
             var defaultContent = "Hello World from a Fake File";
             var defaultFileName = "DefaultImageReview.jpg";
@@ -167,6 +148,13 @@
 
             var imageFileDefault = imageFileDefaultMock.Object;
 
+            var fileId = await this.filesService.UploadToFileSystemAsync(imageFileDefault, "test");
+            await this.imageRepository.AddAsync(new ImageToReview { FileId = fileId });
+            await this.imageRepository.SaveChangesAsync();
+        }
+
+        private void InitializeFields()
+        {
             var imageFileMock = new Mock<IFormFile>();
             var content = "Hello World from a Fake File";
             var fileName = "test.jpg";
@@ -179,28 +167,7 @@
             imageFileMock.Setup(_ => _.FileName).Returns(fileName);
             imageFileMock.Setup(_ => _.Length).Returns(ms.Length);
 
-            var imageFile = imageFileMock.Object;
-
-            var fileId = await this.filesService.UploadToFileSystem(imageFileDefault, "test");
-            await this.imageRepository.AddAsync(new ImageToReview { FileId = fileId });
-            await this.imageRepository.SaveChangesAsync();
-
-            var model = new ReviewInputModel
-            {
-                Name = "Test",
-                Description = "Lorem Ipsum is simply dummy text.",
-                ImageFile = imageFile,
-            };
-
-            await this.informationService.AddRewievAsync(model);
-
-            var currentModel = await this.reviewRepository.All().FirstOrDefaultAsync();
-
-            await this.informationService.DeleteReviewAsync(currentModel.Id);
-
-            var countReview = this.informationService.GetAllReviewAsync().Result.Count;
-
-            Assert.Equal(0, countReview);
+            this.imageFile = imageFileMock.Object;
         }
     }
 }
